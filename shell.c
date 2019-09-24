d90caf527761e9b604fd9b41437d36df9d65b630
shivam <gargshivam482@gmail.com>
2019-09-24 22:29:25 +0530

    shell edited

---
+++ shell.c
@@ -0,0 +1,280 @@
+#include<stdio.h> 
+#include<string.h> 
+#include<stdlib.h> 
+#include<unistd.h> 
+#include<sys/types.h> 
+#include<sys/wait.h> 
+#include<readline/readline.h> 
+#include<readline/history.h>
+//for retrieving hostname and ip address
+#include <errno.h> 
+#include <netdb.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <arpa/inet.h> 
+
+#define MAXCOM 1000 // max number of letters to be supported 
+#define MAXLIST 100 // max number of commands to be supported 
+
+// Clearing the shell using escape sequences 
+#define clear() printf("\033[H\033[J") 
+
+// Greeting shell during startup 
+void init_shell() 
+{ 
+	printf("\n"); 
+	char* username = getenv("USER");
+	char hostbuf[25];
+	char *IPbuffer;
+	struct hostent *host_entry;
+	gethostname(hostbuf,sizeof(hostbuf));
+	host_entry = gethostbyname(hostbuf);
+	IPbuffer =inet_ntoa(*((struct in_addr*)host_entry->h_addr_list[0]));
+	printf("%s your ip address is %s, and you are working on %s",username,IPbuffer,hostbuf);
+
+	sleep(1);
+} 
+
+// Function to take input 
+int takeInput(char* str) 
+{ 
+	char* buf; 
+	char input[]="";
+	buf = readline(input); 
+	if (strlen(buf) != 0) { 
+		add_history(buf); 
+		strcpy(str, buf); 
+		return 0; 
+	} else { 
+		return 1; 
+	} 
+} 
+
+// Function to print Current Directory. 
+void printDir() 
+{ 
+	char cwd[1024]; 
+	getcwd(cwd, sizeof(cwd)); 
+	
+	printf("\nssshell:~%s :-$ ",cwd);
+} 
+
+// Function where the system command is executed 
+void execArgs(char** parsed) 
+{ 
+	// Forking a child 
+	pid_t pid = fork(); 
+
+	if (pid == -1) { 
+		printf("\nFailed forking child.."); 
+		return; 
+	} else if (pid == 0) { 
+		if (execvp(parsed[0], parsed) < 0) { 
+			printf("\nCould not execute command.."); 
+		} 
+		exit(0); 
+	} else { 
+		// waiting for child to terminate 
+		wait(NULL); 
+		return; 
+	} 
+} 
+
+// Function where the piped system commands is executed 
+void execArgsPiped(char** parsed, char** parsedpipe) 
+{ 
+	// 0 is read end, 1 is write end 
+	int pipefd[2]; 
+	pid_t p1, p2; 
+
+	if (pipe(pipefd) < 0) { 
+		printf("\nPipe could not be initialized"); 
+		return; 
+	} 
+	p1 = fork(); 
+	if (p1 < 0) { 
+		printf("\nCould not fork"); 
+		return; 
+	} 
+
+	if (p1 == 0) { 
+		// Child 1 executing.. 
+		// It only needs to write at the write end 
+		close(pipefd[0]); 
+		dup2(pipefd[1], STDOUT_FILENO); 
+		close(pipefd[1]); 
+
+		if (execvp(parsed[0], parsed) < 0) { 
+			printf("\nCould not execute command 1.."); 
+			exit(0); 
+		} 
+	} else { 
+		// Parent executing 
+		p2 = fork(); 
+
+		if (p2 < 0) { 
+			printf("\nCould not fork"); 
+			return; 
+		} 
+
+		// Child 2 executing.. 
+		// It only needs to read at the read end 
+		if (p2 == 0) { 
+			close(pipefd[1]); 
+			dup2(pipefd[0], STDIN_FILENO); 
+			close(pipefd[0]); 
+			if (execvp(parsedpipe[0], parsedpipe) < 0) { 
+				printf("\nCould not execute command 2.."); 
+				exit(0); 
+			} 
+		} else { 
+			// parent executing, waiting for two children 
+			wait(NULL); 
+			wait(NULL); 
+		} 
+	} 
+} 
+
+// Help command builtin 
+void openHelp() 
+{ 
+	puts("\n***WELCOME SSSHELL HELP***"
+		"\n   Copyright @ shivam1410"
+		"\n!!!Use the ssshell at your own risk!!!"
+		"\nit supports basic linux commands along with"
+		"\n >hello"
+		"\n >help"
+		"\n >This ssshell can handle improper space and piped command easily"); 
+
+	return; 
+} 
+
+// Function to execute builtin commands 
+int ownCmdHandler(char** parsed) 
+{ 
+	int NoOfOwnCmds = 4, i, switchOwnArg = 0; 
+	char* ListOfOwnCmds[NoOfOwnCmds]; 
+	char* username; 
+
+	ListOfOwnCmds[0] = "exit"; 
+	ListOfOwnCmds[1] = "cd"; 
+	ListOfOwnCmds[2] = "help"; 
+	ListOfOwnCmds[3] = "hello"; 
+
+	for (i = 0; i < NoOfOwnCmds; i++) { 
+		if (strcmp(parsed[0], ListOfOwnCmds[i]) == 0) { 
+			switchOwnArg = i + 1; 
+			break; 
+		} 
+	} 
+
+	switch (switchOwnArg) { 
+	case 1: 
+		printf("\nGoodbye\n"); 
+		exit(0); 
+	case 2: 
+		chdir(parsed[1]); 
+		return 1; 
+	case 3: 
+		openHelp(); 
+		return 1; 
+	case 4: 
+		username = getenv("USER"); 
+		printf("\nHello %s.\n"
+			"\nUse help to know more about our ssshell\n", 
+			username); 
+		return 1; 
+	default: 
+		break; 
+	} 
+
+	return 0; 
+} 
+
+// function for finding pipe 
+int parsePipe(char* str, char** strpiped) 
+{ 
+	int i; 
+	for (i = 0; i < 2; i++) { 
+		strpiped[i] = strsep(&str, "|"); 
+		if (strpiped[i] == NULL) 
+			break; 
+	} 
+
+	if (strpiped[1] == NULL) 
+		return 0; // returns zero if no pipe is found. 
+	else { 
+		return 1; 
+	} 
+} 
+
+// function for parsing command words 
+void parseSpace(char* str, char** parsed) 
+{ 
+	int i; 
+
+	for (i = 0; i < MAXLIST; i++) { 
+		parsed[i] = strsep(&str, " "); 
+
+		if (parsed[i] == NULL) 
+			break; 
+		if (strlen(parsed[i]) == 0) 
+			i--; 
+	} 
+} 
+
+int processString(char* str, char** parsed, char** parsedpipe) 
+{ 
+
+	char* strpiped[2]; 
+	int piped = 0; 
+
+	piped = parsePipe(str, strpiped); 
+
+	if (piped) { 
+		parseSpace(strpiped[0], parsed); 
+		parseSpace(strpiped[1], parsedpipe); 
+
+	} else { 
+
+		parseSpace(str, parsed); 
+	} 
+
+	if (ownCmdHandler(parsed)) 
+		return 0; 
+	else
+		return 1 + piped; 
+} 
+
+int main() 
+{ 
+	char inputString[MAXCOM], *parsedArgs[MAXLIST]; 
+	char* parsedArgsPiped[MAXLIST]; 
+	int execFlag = 0; 
+	init_shell(); 
+
+	while (1) { 
+		// print shell line 
+		printDir(); 
+		// take input 
+		if (takeInput(inputString)) 
+			continue; 
+		// process 
+		execFlag = processString(inputString, 
+		parsedArgs, parsedArgsPiped); 
+		// execflag returns zero if there is no command 
+		// or it is a builtin command, 
+		// 1 if it is a simple command 
+		// 2 if it is including a pipe. 
+
+		// execute 
+		if (execFlag == 1) 
+			execArgs(parsedArgs); 
+
+		if (execFlag == 2) 
+			execArgsPiped(parsedArgs, parsedArgsPiped); 
+	} 
+	return 0; 
+} 
+
